60e42aac52b17dcfe8f1d2011eaed326c637d034
Benjamin <benjamin.laschiazza@mail.mcgill.ca>
2015-10-03 16:22:26 -0400

    data

--- server/modules/generate-accounts.js
+++ server/modules/generate-accounts.js
@@ -7,81 +7,9 @@ let administrators = [
 ];
 
 let generateAccounts = () => {
-  let fakeUserCount = 2,
-  usersExist    = _checkIfAccountsExist( administrators.length + fakeUserCount );
-
-  if ( !usersExist ) {
-    _createUsers( administrators );
-    _createUsers( _generateFakeUsers( ) );
-  }
 
   
 };
-
-let _checkIfAccountsExist = ( count ) => {
-  let userCount = Meteor.users.find().count();
-  return userCount < count ? false : true;
-};
-
-let _createUsers = ( users ) => {
-  for ( let i = 0; i < users.length; i++ ) {
-    let user       = users[ i ],
-        userExists = _checkIfUserExists( user.email );
-
-    if ( !userExists ) {
-      _createUser( user );
-    }
-  }
-};
-
-let _checkIfUserExists = ( email ) => {
-  return Meteor.users.findOne( { 'emails.address': email } );
-};
-
-let _createUser = ( user ) => {
-  Accounts.createUser({
-    email: user.email,
-    password: user.password,
-    profile: {
-      name: user.name
-    }
-  });
   
-  var id = Meteor.users.findOne( { 'emails.address': user.email } )._id;
-  
-  UserLocation.insert({
-  	user_id: id,
-  	location: 
-  			{
-  				type: 'Point',
-  				coordinates: [45, 64]
-  			}
-  });
-  
-  Interest.insert({
-  	user_id: id,
-  	interests: ['Meteor', 'Finance']
-  });
-  
-};
-
-let _generateFakeUsers = ( ) => {
-  let users = [];
-
-	users.push({
-      name: { first: "Benjamin", last: "La Schiazza" },
-      email: "ben@ben.com",
-      password: 'password'
-    });
-    
-    users.push({
-      name: { first: "John", last: "Doe" },
-      email: "john@john.com",
-      password: 'password'
-    });
-  
-  return users;
-};
-
 Modules.server.generateAccounts = generateAccounts;
 
--- server/modules/startup.js
+++ server/modules/startup.js
@@ -1,6 +1,32 @@
 let startup = () => {
   _setBrowserPolicies();
-  _generateAccounts();
+  
+if(Meteor.users.find().count() < 1)
+{
+  Accounts.createUser({
+    email: "ben@ben.com",
+    password: "password",
+    profile: {
+      name: { first: 'ben', last: 'ben' },
+    }
+  });
+  
+  
+  UserLocation.insert({
+  	user_id: Meteor.users.findOne( { 'emails.address': "ben@ben.com" } )._id,
+  	location: 
+  			{
+  				type: 'Point',
+  				coordinates: [45, 64]
+  			}
+  });
+  
+  Interest.insert({
+  	user_id: Meteor.users.findOne( { 'emails.address': "ben@ben.com" } )._id,
+  	interests: ['Meteor', 'Finance']
+  });
+}
+
 };
 
 var _setBrowserPolicies = () => {};
@@ -9,12 +35,3 @@ var _generateAccounts = () => Modules.server.generateAccounts();
 
 Modules.server.startup = startup;
 
-//Insert dummy data
-var flag = true;
-
-if(flag)
-{
-
-
-
-}
