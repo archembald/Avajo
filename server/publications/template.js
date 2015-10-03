be57fbbabadc571dd1ab776b33363e703d2c50e1
Benjamin <benjamin.laschiazza@mail.mcgill.ca>
2015-10-03 14:27:02 -0400

    basic publications

--- server/publications/template.js
+++ server/publications/template.js
@@ -1,3 +1,21 @@
-Meteor.publish( 'template', function() {
-  return Collection.find( { 'owner': this.userId }, { fields: { 'owner': 1 } } );
+Meteor.publish('template', function() {
+	return Collection.find( { 'owner': this.userId }, { fields: { 'owner': 1 } } );
 });
+
+Meteor.publish('userlocation', function() {
+	return Account.find( { 'user_id': this.userId } );
+});
+
+Meteor.publish('interests', function() { 
+	return Interest.find( { 'user_id': this.userId } );
+}); 
+
+Meteor.publish('messages', function(chatId){
+	return Message.find( { 'chat_id': chatId } );
+});
+
+Meteor.publish('hangout', function(){
+	return Hangout.find( {'users': this.userId } );
+});
+
+
